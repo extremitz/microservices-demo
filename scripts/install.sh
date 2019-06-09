5ee2ac91c01a1ae102f0045847885d895ce5309e
Suwitcha <extremitz@gmail.com>
2019-06-09 16:08:37 +0700

    add config

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,5 +4,5 @@
 # Install kubernetes and set config
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-chmod +x scripts/install.sh
 sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl --kubeconfig config version
