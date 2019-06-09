2fb253e94cd9023f034e251522953dec21721073
Suwitcha <extremitz@gmail.com>
2019-06-09 16:24:42 +0700

    test

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,5 +5,6 @@
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-kubectl --kubeconfig scripts/config version
+cp config ~/.kube/
+kubectl version
 kubectl apply -f deploy/kubernetes/complete-demo.yaml
